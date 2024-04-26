449ce94a5bc38d0d128f512bb3a829dce57ad7e2
ABHISHEK KUMAR <abhishekblog6315@gmail.com>
2024-04-26 12:08:17 +0530

    backend added

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors())
 
 function getRandomNumber(max) {
     return Math.floor(Math.random() * max);
   }
 
- app.get('/notifications' , (req, res) => {
+ app.get('/notification' , (req, res) => {
     console.log("notifications sections")
     res.json({
         network: getRandomNumber(100),
